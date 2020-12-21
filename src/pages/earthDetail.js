84db07d6edbee2c886a4b3ad49c2941fc497bd54
juergenmayer89 <juergen.mayer@stud.th-deg.de>
2020-12-21 17:37:52 +0100

    Earth Detail site and new Suntexture

---
+++ src/pages/earthDetail.js
@@ -0,0 +1,69 @@
+import * as THREE from '../../lib/threejs/r122/build/three.module.js';
+import {
+  createEarthCloud,
+  createEarthMesh,
+} from '../modules/earth.js';
+
+// renderer
+const canvas = document.querySelector('canvas');
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.shadowMap.enabled = true;
+
+// initialize scene and camera
+const scene = new THREE.Scene();
+const fov = 45;
+const aspect = canvas.innerWidth / canvas.innerHeight; // the canvas default
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(-2.5, 0, 4);
+
+// Add Background
+const loader = new THREE.TextureLoader();
+const texture = loader.load('src/textures/background.jpg', () => {
+  const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+  rt.fromEquirectangularTexture(renderer, texture);
+  scene.background = rt;
+});
+
+// Add Light
+const light = new THREE.AmbientLight(0x404040); // soft white light
+const directionalLight = new THREE.DirectionalLight(0xf39f8f, 0.75);
+directionalLight.position.set(-1, 0, 0);
+scene.add(light, directionalLight);
+
+// Add earth with clouds to scene
+const earthMesh = createEarthMesh();
+earthMesh.rotation.set(0, Math.PI, 0);
+const cloudMesh = createEarthCloud();
+earthMesh.add(cloudMesh);
+scene.add(earthMesh);
+
+// handle browser resize
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize =
+    canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+// Add animations to elements in scene
+const render = () => {
+  earthMesh.rotation.y += 0.002;
+  cloudMesh.rotation.y += 0.004;
+
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  renderer.render(scene, camera);
+};
+
+renderer.setAnimationLoop(render);
