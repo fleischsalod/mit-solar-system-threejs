d987af0b29c8ee291a8ac070939d7b6bc22c243d
juergenmayer89 <juergen.mayer@stud.th-deg.de>
2020-12-27 18:45:35 +0100

    Einzelansicht final

--- src/modules/sun.js
+++ src/modules/sun.js
@@ -3,6 +3,12 @@
  */
 
 import {
+  WebGLRenderer,
+  RepeatWrapping,
+  TextureLoader,
+  Vector3,
+  Vector2,
+  ShaderMaterial,
   RingBufferGeometry,
   VideoTexture,
   Mesh,
@@ -10,6 +16,11 @@ import {
   SphereGeometry,
   DoubleSide,
 } from '../../lib/threejs/r122/build/three.module.js';
+
+import { EffectComposer } from '../../lib/threejs/r122/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from '../../lib/threejs/r122/examples/jsm/postprocessing/RenderPass.js';
+import { FilmPass } from '../../lib/threejs/r122/examples/jsm/postprocessing/FilmPass.js';
+import { BloomPass } from '../../lib/threejs/r122/examples/jsm/postprocessing/BloomPass.js';
 
 import {
   getElementDiameter,
@@ -19,6 +30,7 @@ import {
 
 // basic url to textures of sun
 const BASIC_URL = 'src/textures/sun/';
+// const BASIC_URL2 = 'src/textures/lava';
 
 /**
  * Create mesh of sun
@@ -40,42 +52,42 @@ const createSunMesh = () => {
 
   const mesh = new Mesh(geometry, material);
   mesh.rotation.x = getAxialTiltInRad('sun');
-  const fireRing = createSunFireRing();
-  mesh.add(fireRing);
+  // const fireRing = createSunFireRing();
+  // mesh.add(fireRing);
   return mesh;
 };
 
-const createSunFireRing = () => {
-  const ringsInnerDiameter = getElementDiameter('sun');
-  const ringsOuterDiameter = ringsInnerDiameter + 30;
-  const geometry = new RingBufferGeometry(
-    ringsInnerDiameter,
-    ringsOuterDiameter,
-    64,
-  );
-  var uvs = geometry.attributes.uv.array;
-  // loop and initialization taken from RingBufferGeometry
-  var phiSegments = geometry.parameters.phiSegments || 0;
-  var thetaSegments = geometry.parameters.thetaSegments || 0;
-  phiSegments =
-    phiSegments !== undefined ? Math.max(1, phiSegments) : 1;
-  thetaSegments =
-    thetaSegments !== undefined ? Math.max(3, thetaSegments) : 8;
-  for (var c = 0, j = 0; j <= phiSegments; j++) {
-    for (var i = 0; i <= thetaSegments; i++) {
-      (uvs[c++] = i / thetaSegments), (uvs[c++] = j / phiSegments);
-    }
-  }
-  const video02 = document.getElementById('video02');
-  video02.play();
-  const videoTexture = new VideoTexture(video02);
-  const material = new MeshPhongMaterial({
-    map: videoTexture,
-    side: DoubleSide,
-    transparent: true,
-  });
-  const ringMesh = new Mesh(geometry, material);
-  return ringMesh;
-};
+// const createSunFireRing = () => {
+//   const ringsInnerDiameter = getElementDiameter('sun');
+//   const ringsOuterDiameter = ringsInnerDiameter + 30;
+//   const geometry = new RingBufferGeometry(
+//     ringsInnerDiameter,
+//     ringsOuterDiameter,
+//     64,
+//   );
+//   var uvs = geometry.attributes.uv.array;
+//   // loop and initialization taken from RingBufferGeometry
+//   var phiSegments = geometry.parameters.phiSegments || 0;
+//   var thetaSegments = geometry.parameters.thetaSegments || 0;
+//   phiSegments =
+//     phiSegments !== undefined ? Math.max(1, phiSegments) : 1;
+//   thetaSegments =
+//     thetaSegments !== undefined ? Math.max(3, thetaSegments) : 8;
+//   for (var c = 0, j = 0; j <= phiSegments; j++) {
+//     for (var i = 0; i <= thetaSegments; i++) {
+//       (uvs[c++] = i / thetaSegments), (uvs[c++] = j / phiSegments);
+//     }
+//   }
+//   const video02 = document.getElementById('video02');
+//   video02.play();
+//   const videoTexture = new VideoTexture(video02);
+//   const material = new MeshPhongMaterial({
+//     map: videoTexture,
+//     side: DoubleSide,
+//     transparent: true,
+//   });
+//   const ringMesh = new Mesh(geometry, material);
+//   return ringMesh;
+// };
 
 export { createSunMesh };
--- src/pages/moonDetail.js
+++ src/pages/moonDetail.js
@@ -29,10 +29,162 @@ const directionalLight = new THREE.DirectionalLight(0xf39f8f, 1.5);
 directionalLight.position.set(-1, 0, 0);
 scene.add(light, directionalLight);
 
+//Drawing Lines for Design and Text
+const Line1 = () => {
+  const material = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+  });
+
+  const points = [];
+  points.push(
+    new THREE.Vector3((-582 / 250) * 0.9375, (155 / 250) * 0.9375, 0),
+  );
+  points.push(
+    new THREE.Vector3(
+      (-582 / 250) * 0.9375,
+      (-140 / 250) * 0.9375,
+      0,
+    ),
+  );
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  return line;
+};
+
+const Line2 = () => {
+  const material = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+  });
+
+  const points = [];
+  points.push(
+    new THREE.Vector3((-580 / 250) * 0.9375, (163 / 250) * 0.9375, 0),
+  );
+  points.push(
+    new THREE.Vector3((-200 / 250) * 0.9375, (163 / 250) * 0.9375, 0),
+  );
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  return line;
+};
+
+const Line3 = () => {
+  const material = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+  });
+
+  const points = [];
+  points.push(
+    new THREE.Vector3((-390 / 250) * 0.9375, (80 / 250) * 0.9375, 0),
+  );
+  points.push(
+    new THREE.Vector3(
+      (-390 / 250) * 0.9375,
+      (-100 / 250) * 0.9375,
+      0,
+    ),
+  );
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  return line;
+};
+
+const Line4 = () => {
+  const material = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+  });
+
+  const points = [];
+  points.push(
+    new THREE.Vector3((-387 / 250) * 0.9375, (60 / 250) * 0.9375, 0),
+  );
+  points.push(
+    new THREE.Vector3((-387 / 250) * 0.9375, (-80 / 250) * 0.9375, 0),
+  );
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  return line;
+};
+
+const Line5 = () => {
+  const material = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+  });
+
+  const points = [];
+  points.push(new THREE.Vector3((-387 / 250) * 0.9375, 0, 0));
+  points.push(new THREE.Vector3((-150 / 250) * 0.9375, 0, 0));
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  return line;
+};
+
+const Line6 = () => {
+  const material = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+  });
+
+  const points = [];
+  points.push(
+    new THREE.Vector3((-290 / 250) * 0.9375, (-10 / 250) * 0.9375, 0),
+  );
+  points.push(
+    new THREE.Vector3((-290 / 250) * 0.9375, (10 / 250) * 0.9375, 0),
+  );
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  return line;
+};
+
+const Line7 = () => {
+  const material = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+  });
+
+  const points = [];
+  points.push(
+    new THREE.Vector3((-340 / 250) * 0.9375, (-50 / 250) * 0.9375, 0),
+  );
+  points.push(
+    new THREE.Vector3((-340 / 250) * 0.9375, (50 / 250) * 0.9375, 0),
+  );
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  return line;
+};
+
 // Add earth with clouds to scene
 const moonMesh = createEarthMoon();
+const line1 = Line1();
+const line2 = Line2();
+const line3 = Line3();
+const line4 = Line4();
+const line5 = Line5();
+const line6 = Line6();
+const line7 = Line7();
 moonMesh.rotation.set(0, Math.PI, 0);
-scene.add(moonMesh);
+scene.add(moonMesh, line1, line2, line3, line4, line5, line6, line7);
 
 // handle browser resize
 function resizeRendererToDisplaySize(renderer) {
